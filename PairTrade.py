54a315be30f39c16cd52827ff456887a113f99c1
abreroy <77888445+abreroy@users.noreply.github.com>
2024-02-19 17:19:26 +0100

    Update PairTrade.py

--- PairTrade.py
+++ PairTrade.py
@@ -7,7 +7,7 @@ from alpaca.trading.client import TradingClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from alpaca.trading.requests import OrderRequest, GetCalendarRequest, ClosePositionRequest
-#note test 5
+#note test 1
 
 class PairTrade:
 
