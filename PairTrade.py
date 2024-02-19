37455439f7d65a1612b913b8519c175d726b8a49
abreroy <thore.pick@gmail.com>
2024-02-19 17:17:04 +0100

    test

--- PairTrade.py
+++ PairTrade.py
@@ -7,7 +7,7 @@ from alpaca.trading.client import TradingClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from alpaca.trading.requests import OrderRequest, GetCalendarRequest, ClosePositionRequest
-#note test 3
+#note test 4
 
 class PairTrade:
 
